187bc0ed97626b7577b7ba6fad3b9cee73f00b0a
Lea247240 <HomanovaLeony@seznam.cz>
2024-04-08 10:21:37 +0200

    oprava

--- searching.py
+++ searching.py
@@ -19,10 +19,43 @@ def read_data(file_name, field):
 
     return data.get(field)
 
+def linear_search(sekvence, number):
+
+    position = []
+    count = 0
+    for i, num in enumerate(sekvence):
+        if num == number:
+            position.append(i)
+            count = count + 1
+    slovnik = {"Possitions":position,"count":count}
+    return slovnik
+
+def pattern_search(my_sequence, vzor):
+    delka = len(vzor)
+    position = set()
+    for i in range(len(my_sequence)-delka):
+        prvek = my_sequence[i:delka + i]
+        if prvek == vzor:
+            position.add(i)
+
+#def binary_search(my_numbers, number):
+
+
+
+
+
 def main():
     #pass
     sequentationl_data = read_data("sequential.json", "unordered_numbers")
+    sequentationl_data1 = read_data("sequential.json","dna_sequence")
+    sequentationl_data2 = read_data("sequential.json",  "ordered_numbers")
     print(sequentationl_data)
+    linear = linear_search(sequentationl_data, 1)
+    print(linear)
+    porovnani = pattern_search(sequentationl_data1, "ATA")
+    print(porovnani)
+    #binary = pattern_search(sequentationl_data2,3)
+    #print(binary)
 
 if __name__ == '__main__':
     main()
