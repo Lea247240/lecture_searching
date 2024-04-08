46f85356c1f6ed9b22ca6cb4b7251154c035f078
Lea247240 <HomanovaLeony@seznam.cz>
2024-04-08 09:56:55 +0200

    oprava

--- searching.py
+++ searching.py
@@ -1,22 +1,28 @@
 import os
-
-# get current working directory path
-cwd_path = os.getcwd()
-
+import json
 
 def read_data(file_name, field):
     """
-    Reads json file and returns sequential data.
-    :param file_name: (str), name of json file
-    :param field: (str), field of a dict to return
-    :return: (list, string),
-    """
-    file_path = os.path.join(cwd_path, file_name)
+    Reads csv file and returns numeric data.
 
+    :param file_name: (str), name of CSV file
+    :return: (dict), dictionary with numeric data, keys - csv column names, values - numbers in each column
+    """
+    cwd_path = os.getcwd()
+    file_path = os.path.join(cwd_path, file_name)
+    with open(file_path, mode="r") as f:
+        allowed_key = json.load(f)
+        if field not in allowed_key:
+            return None
+    with open(file_name, "r") as f:
+        data = json.load(f)
+
+    return data.get(field)
 
 def main():
-    pass
-
+    #pass
+    sequentationl_data = read_data("sequential.json", "unordered_numbers")
+    print(sequentationl_data)
 
 if __name__ == '__main__':
     main()
